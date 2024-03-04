156008d232d4886fc4b0f169b05019190fdc8d86
mikemed023 <35246104+mikemed023@users.noreply.github.com>
2024-03-03 23:39:04 -0500

    Add files via upload

---
+++ pwmled2_CC3220S_LAUNCHXL_nortos_ticlang/MCU+Image/pwmled2.d
@@ -0,0 +1,18 @@
+# FIXED
+
+pwmled2.o: ../pwmled2.c \
+ C:/ti/simplelink_cc32xx_sdk_7_10_00_13/kernel/nortos/posix/unistd.h \
+ C:/ti/simplelink_cc32xx_sdk_7_10_00_13/kernel/nortos/posix/sys/types.h \
+ C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/drivers/PWM.h \
+ syscfg/ti_drivers_config.h \
+ C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/devices/DeviceFamily.h
+
+C:/ti/simplelink_cc32xx_sdk_7_10_00_13/kernel/nortos/posix/unistd.h:
+
+C:/ti/simplelink_cc32xx_sdk_7_10_00_13/kernel/nortos/posix/sys/types.h:
+
+C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/drivers/PWM.h:
+
+syscfg/ti_drivers_config.h:
+
+C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/devices/DeviceFamily.h:
